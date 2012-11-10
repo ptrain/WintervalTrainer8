b9288902119de636cd39873e38a3448db171902b
Hayden Chudy <hjc1710@gmail.com>
2012-11-10 04:03:45 -0500

    Landscape UI Finished
    All landscape elements placed and positioned.
    
    Updated app manifest to have app description and title.
    
    Added the data and some helper methods/event handlers.

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Windows.Media;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
@@ -23,6 +25,17 @@ namespace HelloWorld
     {
         private int numWrong = 0;
         private int numRight = 0;
+
+        /// <summary>
+        /// This variable is used to determine if we are waiting for our sound events to finish.
+        ///   If we are, this is true and all other events are ignored. If not, they are allowed.
+        /// </summary>
+        private bool holdForSound = false;
+
+        //private Interval lastInt = null;
+
+        List<Interval> intervals = new List<Interval>();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -41,6 +54,8 @@ namespace HelloWorld
         {
             ansWrongNum.Text = numWrong.ToString();
             ansRightNum.Text = numRight.ToString();
+            this.populateIntervals();
+            //this
         }
 
         /// <summary>
@@ -51,6 +66,7 @@ namespace HelloWorld
         /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
         protected override void SaveState(Dictionary<String, Object> pageState)
         {
+            
         }
 
         /*private void Button_Click(object sender, RoutedEventArgs e)
@@ -60,9 +76,71 @@ namespace HelloWorld
 
         private void Play_Scale(object sender, RoutedEventArgs e)
         {
-            //play a sound
+            // See if we can play a scale
+            if (holdForSound)
+            {
+                return;
+            }
+            else
+            {
+                holdForSound = true;
+            }
+            // First play a sound
+
+            // Now set the interval that was for later
+            //lastInt = 
+            
+            
             //return null;
+            //ansWrongNum.Text = (theIntervals.SelectedItem as Interval).Name;
+
+
+
+
+
+
+
+
+            //////////THIS LOGIC HAS TO BE CHANGED
+
+            ///// CHANGE THIS FUCKING LOGIC
+
+            ///// THIS IS IN HERE SO WE DON'T FUCK UP LATER, BUT IF WE LEAVE IT WE WILL
+            holdForSound = false;
+            /////those are some angry FUCKING COMMENTS!!!!!!!!!!!!!! LISTEN TO THEM!!!!!!!!!!!!
+            //REMOVE THIS WHEN WE HAVE CODE THAT CLEANS UP AFTER 
+        }
+
+        private void populateIntervals()
+        {
+            intervals.Add(new Interval() { Name = "m2" });
+            intervals.Add(new Interval() { Name = "M2" });
+            intervals.Add(new Interval() { Name = "m3" });
+            intervals.Add(new Interval() { Name = "M3" });
+            intervals.Add(new Interval() { Name = "P4" });
+            intervals.Add(new Interval() { Name = "P5" });
+            intervals.Add(new Interval() { Name = "m6" });
+            intervals.Add(new Interval() { Name = "M6" });
+            intervals.Add(new Interval() { Name = "m7" });
+            intervals.Add(new Interval() { Name = "M7"});
+            intervals.Add(new Interval() { Name = "P8"});
+
+            var res = from i in intervals group i by i.Name into grp orderby grp.Key select grp;
+            cvsInts.Source = res;
+        }
+
+        private void Submit_Answer(object sender, RoutedEventArgs e)
+        {
+            //submit answer and return true or false
+            var ans = (theIntervals.SelectedItem as Interval);
 
         }
+    }
+
+    public class Interval
+    {
+        public string Name { get;  set; }
+        //public string File { get; set; }
+
     }
 }
